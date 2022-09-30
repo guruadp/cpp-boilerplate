34f329d77dc067c4bb2e44b4f74411d6d5ffe67a
guruadp <gurunandhanadp@gmail.com>
2022-09-29 23:48:38 -0400

    added PID controller class

--- app/main.cpp
+++ app/main.cpp
@@ -1,8 +1,39 @@
+/**
+ *  @copyright (c) 2022 GuruNandhan
+ *  @file    main.cpp
+ *  @author  GuruNandhan
+ *  @brief Test Driven Development
+ *
+ */
+
 #include <iostream>
-#include <lib.hpp>
+
+class PIDController{
+
+private:
+    int kp, ki, kd;
+
+public:
+    /**
+     * @brief 
+     * 
+     * @param setPoint
+     * @param actualVelocity
+     * 
+     * @return postion as 
+     */
+
+    float computeVelocity(float setPoint, float actualVelocity){
+        float newVelocity = 10;
+        return newVelocity;
+    }
+};
 
 int main()
 {
-    dummy();
+    PIDController controller;
+
+    float newVelocity = controller.computeVelocity(5, 15);
+    std::cout<<newVelocity;
     return 0;
 }
